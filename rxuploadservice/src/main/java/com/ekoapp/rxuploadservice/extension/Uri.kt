672ac242bb7f9681c750d598179874b3ace68748
Pan <theptai@ekoapp.com>
2021-03-12 14:48:51 +0700

    context is required

--- rxuploadservice/src/main/java/com/ekoapp/rxuploadservice/extension/Uri.kt
+++ rxuploadservice/src/main/java/com/ekoapp/rxuploadservice/extension/Uri.kt
@@ -1,9 +1,10 @@
 package com.ekoapp.rxuploadservice.extension
 
+import android.content.Context
 import android.net.Uri
 import com.ekoapp.rxuploadservice.model.FileProperties
 import io.reactivex.Flowable
 
-fun Uri.upload(id: String? = null, url: String): Flowable<FileProperties> {
+fun Uri.upload(context: Context, url: String, id: String? = null): Flowable<FileProperties> {
     return Flowable.never()
 }
